12be7e4100fe350b9f45bb6615edaceeeb54b7f9
vidklopcic <klopcic.vid@gmail.com>
2021-05-04 19:27:08 +0200

    file uploads are working

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+import base64
+import os
 from pathlib import Path
+import pyvips
 from aiohttp import web, ClientSession
 from aiohttp_tus import setup_tus
 import argparse
-
 from aiohttp_tus.data import Resource
 
 parser = argparse.ArgumentParser(description='aiohttp implementation of TUS file server')
@@ -12,17 +14,37 @@ parser.add_argument('--url', type=str, default='/upload/')
 parser.add_argument('--callback', type=str, default='http://localhost:8000/uploaded/')
 parser.add_argument('--dir', type=str, required=True)
 args = parser.parse_args()
+args.dir = Path(args.dir)
 
 
 async def on_upload_done(request: web.Request, resource: Resource, path: Path):
+    metadata = {v.split(' ')[0]: base64.b64decode(v.split(' ')[1]).decode('utf-8') for v in
+                resource.metadata_header.split(',')}
+
+    path = str(path)
+    if 'path' in metadata:
+        destination = args.dir / metadata.get('path')
+        destination.parent.mkdir(parents=True, exist_ok=True)
+        destination = str(destination)
+        os.rename(path, destination)
+        path = destination
+
+    if 'preview' in metadata:
+        preview = args.dir / metadata.get('preview')
+        preview.parent.mkdir(parents=True, exist_ok=True)
+        preview = str(preview)
+        thumbnail: pyvips.Image = pyvips.Image.thumbnail(path, 255)
+        thumbnail.write_to_file(preview)
+
     if not args.callback:
         return
+
     async with ClientSession() as session:
         callback_data = {
             'filename': resource.file_name,
             'size': resource.file_size,
-            'path': str(path),
-            'id': request.headers.get('id'),
+            'path': path,
+            'metadata': request.headers,
         }
         async with session.post(args.callback, data=callback_data) as resp:
             pass
@@ -30,9 +52,9 @@ async def on_upload_done(request: web.Request, resource: Resource, path: Path):
 
 app = setup_tus(
     web.Application(),
-    upload_path=Path(args.dir),
+    upload_path=args.dir,
     upload_url=args.url,
-    on_upload_done=on_upload_done
+    on_upload_done=on_upload_done,
 )
 
 if __name__ == '__main__':

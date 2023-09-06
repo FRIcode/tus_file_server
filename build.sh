0f0c99fd8d1335649fd7c7c9b531ad55633cf08d
vidklopcic <klopcic.vid@gmail.com>
2023-09-06 12:02:21 +0200

    jwt, build with version

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Read the version from version.txt
+version=$(cat version.txt)
+
+# Check if the version is empty
+if [[ -z "$version" ]]; then
+  echo "Error: version.txt is empty or missing."
+  exit 1
+fi
+
+# Export VERSION as environment variable
+export VERSION=$version
+
+# Build and tag all services
+docker-compose build
